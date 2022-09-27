660609f3966e34890183b5e4387aa2c898eb47c5
vpontak <pontak.v@gmail.com>
2022-09-27 09:40:11 +0200

    123123123

---
+++ main.js
@@ -0,0 +1,6 @@
+function myFunction() { 
+    document.getElementById("book_overview_demo").innerHTML = "Book overview";
+  }
+function Signup() {
+  document.getElementById("signup_demo").innerHTML = "sign-up";
+}
